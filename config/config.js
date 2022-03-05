a4e3fb25e50dbd68f8761be53a02102d390aa8d2
Saravana kumar <saravana@techlify.com>
2022-03-05 08:32:18 -0400

    Configuration setup

---
+++ config/config.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const convict = require('convict');
+
+// Define a schema
+var config = convict({
+    env: {
+      doc: 'The application environment.',
+      format: ['production', 'development', 'test'],
+      default: 'development',
+      env: 'NODE_ENV'
+    },
+    port: {
+      doc: 'The port to bind.',
+      format: 'port',
+      default: 4000,
+      env: 'PORT',
+      arg: 'port'
+    },
+    db: {
+      host: {
+        doc: 'Database host name/IP',
+        format: '*',
+        default: 'localhost'
+      },
+      name: {
+        doc: 'Database name',
+        format: String,
+        default: 'postgres'
+      },
+      user_name: {
+        doc: 'Database user name',
+        format: String,
+        default: 'root'
+      },
+      password: {
+        doc: 'Database user password',
+        format: String,
+        default: 'saravana7',
+      },
+      port: {
+        doc: 'Database user password',
+        format: Number,
+        default: 5432,
+      }
+    }
+  });
+  
+
+// Load environment dependent configuration
+var env = config.get('env');
+config.loadFile(`./config/${env}.json`);
+
+// Perform validation
+config.validate({allowed: 'strict'});
+
+module.exports = config;
