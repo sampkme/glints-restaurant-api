828ba46ed33bd24baa7c18c63619440b0d32d300
Saravana kumar <saravana@techlify.com>
2022-03-05 10:04:48 -0400

    Database configs isolated

--- src/controllers/restaurant-controller.js
+++ src/controllers/restaurant-controller.js
@@ -1,16 +1,7 @@
-const { Pool } = require('pg');
-var config = require('../config/config.js');
-
-const pool = new Pool({
-    user: config.get('db.user_name'),
-    host: config.get('db.host'),
-    database: config.get('db.name'),
-    password: config.get('db.password'),
-    port: config.get('db.port'),
-});
+var db = require('../config/database');
 
 const getRestaurants = (req, res) => {
-    pool.query('SELECT * FROM restaurants ORDER BY id ASC').then(results => {
+    db.query('SELECT * FROM restaurants ORDER BY id ASC').then(results => {
         res.status(200).json(results.rows);
     }).catch(err => {
         res.sendStatus(500);
@@ -19,7 +10,7 @@ const getRestaurants = (req, res) => {
 }
 
 const getRestaurant = (req, res) => {
-    pool.query('SELECT * FROM restaurants WHERE id=$1', [req.params.id]).then(results => {
+    db.query('SELECT * FROM restaurants WHERE id=$1', [req.params.id]).then(results => {
         res.status(200).json(results.rows[0]);
     }).catch(err => {
         res.sendStatus(500);
