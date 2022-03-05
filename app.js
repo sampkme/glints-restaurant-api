00cebb03a365190dc8013c88d9d0407abf129bbe
Saravana kumar <saravana@techlify.com>
2022-03-05 08:32:32 -0400

    Basic Db setup

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
+const { Pool } = require('pg');
+var config = require('./config/config.js');
+
+const pool = new Pool({
+    user: config.get('db.user_name'),
+    host: config.get('db.host'),
+    database: config.get('db.name'),
+    password: config.get('db.password'),
+    port: config.get('db.port'),
+});
+
 
 // Log transactions
 app.use(morgan('combined'));
 
 // Routes
 app.get("/", function (req, res) {
-    res.send("Welcome to the world of science fiction, conflicting theories, fantasies and some eccentric nerds!")
+    pool.query('SELECT * FROM horrors', (error, results) => {
+        if(error) {
+            res.send(error);
+            res.sendStatus(500);
+            res.end();
+        }
+        res.status(200).json(results.rows);
+    });
+
 });
 
 // Server listening to 3000
-app.listen(3000, function () {
+app.listen(config.get('port'), function () {
     console.log("SERVER STARTED ON localhost:3000");
 })
