ee49fffe18a5dbb63baa7383262be361edeeab20
Saravana kumar <saravana@techlify.com>
2022-03-05 04:56:10 -0400

    installed pg and morgan for logging

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+
+// Log transactions
+app.use(morgan('combined'));
 
 // Routes
 app.get("/", function (req, res) {
     res.send("Welcome to the world of science fiction, conflicting theories, fantasies and some eccentric nerds!")
 });
 
-// Server listen
+// Server listening to 3000
 app.listen(3000, function () {
     console.log("SERVER STARTED ON localhost:3000");
 })
