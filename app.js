5316fa604b1d60795e80b66c8e380be9d7325539
Saravana kumar <saravana@techlify.com>
2022-03-05 10:21:05 -0400

    Cors Setup

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const app = express();
 const morgan = require('morgan');
 var config = require('./src/config/config');
 const router = require("./src/routes/index");
+const cors = require('cors');
 
 // Log transactions
 app.use(morgan('combined'));
 
 // Route
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
     res.send("Welcome to the Restaurant API project. Please use /api as following specific urls EX: /api/restaurants");
 });
 
-app.use('/api', router);
+app.use('/api', cors(), router);
 
 // Server listening to 3000
 app.listen(config.get('port'), function () {
